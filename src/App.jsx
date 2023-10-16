40be089c9e7c4f44c75d48c9ce6d0b79817d4895
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-16 20:04:03 +0700

    Create Question.jsx component and move some props around

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import data from "./data";
-import SingleQuestion from "./SingleQuestion";
+import Questions from "./Questions";
 
 const App = () => {
   const [questions, setQuestions] = useState(data);
 
   return (
     <main>
-      <SingleQuestion questions={questions} />
+      <Questions questions={questions} />
     </main>
   );
 };
