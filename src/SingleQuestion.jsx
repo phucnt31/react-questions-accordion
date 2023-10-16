220e1e5e6a9de7974103e6cfaf83a7a42c5b1795
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-16 20:08:20 +0700

    Display question and create a button to toggle

--- src/SingleQuestion.jsx
+++ src/SingleQuestion.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
-const SingleQuestion = () => {
-  return <div>SingleQuestion</div>;
+const SingleQuestion = ({ id, title, info }) => {
+  return (
+    <article className="question">
+      <header>
+        <h5>{title}</h5>
+        <button type="button" className="question-btn">
+          <AiOutlinePlus />
+        </button>
+      </header>
+
+      <p>{info}</p>
+    </article>
+  );
 };
 
 export default SingleQuestion;
